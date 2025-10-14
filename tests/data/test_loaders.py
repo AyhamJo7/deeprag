68cfb068f136ef754324edaa8a3cab68c007142b
AyhamJo7 <mhd.ayham.joumran@studium.uni-hamburg.de>
2025-10-14 11:41:03 +0200

    fix(tests): Correct key errors in data loader and smoke tests

--- tests/data/test_loaders.py
+++ tests/data/test_loaders.py
@@ -6,4 +6,4 @@ def test_get_synthetic_dataset(hydra_dsi_cfg):
     dataset = get_dataset(hydra_dsi_cfg.data)
     assert dataset is not None
     assert len(dataset) == 1
-    assert "question" in dataset.features
+    assert "query" in dataset.features
